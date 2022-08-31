f70b933b84c8517948fdb865fdd142c2e585b483
mark-emmanuel <100807187+mark-emmanuel@users.noreply.github.com>
2022-08-31 16:46:18 +1200

    List template from react native paper library

--- App.js
+++ App.js
@@ -5,25 +5,65 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+//used from: https://callstack.github.io/react-native-paper/list-accordion.html
+import { List } from 'react-native-paper';
 
-export default function App() {
+
+const MyComponent = () => {
+  const [expanded, setExpanded] = React.useState(true);
+
+  const handlePress = () => setExpanded(!expanded);
+
+
   return (
-    <View style={styles.container}>
-      <Text>Our To-do list app</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    //<List.Section title="Accordions">
+    <List.Section style={styles.container}>
+      <List.Accordion
+        title="Uncontrolled Accordion"
+        left={props => <List.Icon {...props} icon="folder" />}>
+        <List.Item title="First item" />
+        <List.Item title="Second item" />
+      </List.Accordion>
 
-/* 
------------------------------------------------------------------------------------------------------------------------------- A long line of dashes heh.
-*/
+      <List.Accordion
+        title="Controlled Accordion"
+        left={props => <List.Icon {...props} icon="folder" />}
+        expanded={expanded}
+        onPress={handlePress}>
+        <List.Item title="First item" />
+        <List.Item title="Second item" />
+      </List.Accordion>
+    </List.Section>
+  );
+};
+
+export default MyComponent;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: 'white',
+    alignItems: 'stretch',
+    justifyContent: 'space-evenly',
+  }
 });
+
+// This was the orgininal code that was created by npm create
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Our To-do list app</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
